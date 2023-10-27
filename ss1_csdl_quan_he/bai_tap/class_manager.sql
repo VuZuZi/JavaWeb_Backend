63aa9c3b3f463eb4ba8e5cba64692c8489681b9c
vuzuzi <dinhvuht01@gmail.com>
2023-10-27 19:26:57 +0700

    upss1

---
+++ ss1_csdl_quan_he/bai_tap/class_manager.sql
@@ -0,0 +1,12 @@
+create database class_manager;
+use class_manager;
+create table class(
+	id int,
+    name varchar(255)
+);
+create table teacher(
+	id int,
+    name varchar(255),
+    age int,
+    country varchar(255)
+    );
---
+++ ss1_csdl_quan_he/thuc_hanh/create_drop_database.sql
@@ -0,0 +1,8 @@
+create database my_database;
+create table table_1;
+use my_database;
+create table table_2(
+	id int,
+    name varchar(255)
+);
+drop table table_1;
